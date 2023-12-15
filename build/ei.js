16806923726b6426096c665e62f28283036bc234
Matheus <matheusrodrigueslima33@gmail.com>
2023-12-15 17:02:45 -0300

    coments

--- build/ei.js
+++ build/ei.js
@@ -5,9 +5,12 @@ class carro {
         this.nome = nome;
         this.km = km;
     }
-    kmPorHora(vel, metros) {
-        return vel * metros;
+    kmPorHora(velInicial, velFinal) {
+        return (velInicial + velFinal) / 2;
     }
 }
-const carro1 = new carro("GM", "Camaro", 10000);
-console.log(carro1.kmPorHora(50, 100));
+const camaro = new carro("GM", "Camaro", 10000);
+console.log(camaro.kmPorHora(50, 100));
+const ferrari = new carro("Ferrari", "Spider", 150000);
+console.log(ferrari.nome + " " + ferrari.modelo);
+console.log(ferrari.kmPorHora(10, 300));
--- src/ei.ts
+++ src/ei.ts
@@ -9,11 +9,14 @@ class carro{
         this.km = km
     }
 
-    kmPorHora(vel:number, metros: number) {
-        return vel * metros
+    kmPorHora(velInicial:number, velFinal: number) {
+        return (velInicial + velFinal) / 2
     }
 }
 
-const carro1 = new carro("GM", "Camaro", 10000);
-console.log(carro1.kmPorHora(50, 100));
+const camaro = new carro("GM", "Camaro", 10000);
+console.log(camaro.kmPorHora(50, 100));
 
+const ferrari = new carro("Ferrari", "Spider", 150000);
+console.log(ferrari.nome + " " + ferrari.modelo);
+console.log(ferrari.kmPorHora(10, 300));
